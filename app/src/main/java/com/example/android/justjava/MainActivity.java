1d22ba0cfd0cb1290e86a65dd616aa5d13c5752b
andylonny <andylonny@gmail.com>
2017-03-05 15:13:18 -0300

    Update Android Studio 2.3

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -21,20 +23,24 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the order button is clicked.
+     * @param view
      */
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
         int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        displayMessage(createOrderSummary(price, hasWhippedCream));
     }
 
     /**
      * Create summary of the order
-     *
      * @param price of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
      * @return text summary
      */
-    private String createOrderSummary(int price) {
-        String priceMessage = "Name: Kaptain Kunal " + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank You!";
+    private String createOrderSummary(int price, boolean addWhippedCream) {
+        String priceMessage = "Name: Lyla the Labyrinth" + "\nAdd Whipped Cream? " + addWhippedCream + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank You!";
         return priceMessage;
     }
 
@@ -49,9 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the plus button is clicked.
+     * @param view
      */
     public void increment(View view) {
-        quantity = quantity + 1;
+        quantity += 1;
         displayQuantity(quantity);
     }
 
@@ -59,12 +66,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1;
+        quantity -= 1;
         displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
+     * @param number
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
@@ -73,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method displays the given text on the screen.
+     * @param message
      */
     private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
